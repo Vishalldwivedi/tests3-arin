8a0cb1ad42392b9f2e2f7b046fd00b0099b0d308
vishal dwivedi <88195863+Vishalldwivedi@users.noreply.github.com>
2026-02-24 17:22:35 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambdaaad vishal!')
+        'body': json.dumps('Hello from Lambdaaad vishal singh!')
     }
